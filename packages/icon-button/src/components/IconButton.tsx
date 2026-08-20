886a0f31aa4f2ae380bf062065663fffbdbe9d08
XD <asnewyla@gmail.com>
2026-08-20 11:39:37 +0200

    feat(icon-button): add basic scenarios

---
+++ packages/icon-button/src/components/IconButton.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@xd/button';
+
+export type IconButtonProps = {
+  label: string;
+  icon: React.ReactNode;
+};
+
+export function IconButton({ label, icon }: IconButtonProps) {
+  return (
+    <Button startIcon={icon} aria-label={label}>
+      {label}
+    </Button>
+  );
+}
