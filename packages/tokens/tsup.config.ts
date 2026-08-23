9a09faebc6fbe9f1c8e32f40bb90b5e77a68d29b
XD <asnewyla@gmail.com>
2026-08-23 13:09:28 +0200

    feat(tokens): add different themes

--- packages/tokens/tsup.config.ts
+++ packages/tokens/tsup.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/tokens.css'],
+  entry: [
+    'src/tokens.css',
+    'src/theme-paper.css',
+    'src/theme-sand.css',
+    'src/theme-lavender.css',
+  ],
   clean: true,
 });
